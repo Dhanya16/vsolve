3eac66be942255bdfa6c747d0ded459c5cb05c90
Dhanya16 <117178131+Dhanya16@users.noreply.github.com>
2023-07-20 00:16:48 +0530

    first commit

---
+++ assets/js/header.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const myButton1 = document.getElementById("button");
+    myButton1.addEventListener("click", function() {
+        window.location = "index.html";
+    });
+    const myButton2 = document.getElementById("button1");
+    myButton2.addEventListener("click", function() {
+        window.location = "about.html";
+    });
+    const myButton3 = document.getElementById("button2");
+    myButton3.addEventListener("click", function() {
+        window.location = "contact.html";
+    });
+    const myButton4 = document.getElementById("button3");
+    myButton4.addEventListener("click", function() {
+        window.location = "questions.html";
+    });
+    const myButton5 = document.getElementById("button4");
+    myButton5.addEventListener("click", function() {
+        window.location = "equations.html";
+    });
+    const myButton6 = document.getElementById("button5");
+    myButton6.addEventListener("click", function() {
+        window.location = "graphs.html";
+    });
+    const myButton7 = document.getElementById("button6");
+    myButton7.addEventListener("click", function() {
+        window.location = "theory.html";
+    });
+});
